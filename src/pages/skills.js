d83e3fb7a78d421af8465828420a074a98671384
jungjin-lee90 <dlwjdwls60@naver.com>
2025-01-07 15:47:15 +0900

    skills, card 추가

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,4 +1,5 @@
 import "./skills.css";
+import Card from '../component/card';
 
 const Skills = () => {
   return (
@@ -6,32 +7,34 @@ const Skills = () => {
       <h1 className="skills-title">
         <a name="skills">Tech Skills</a>
       </h1>
-      <div className="skill-card-wrapper">
-
-          <h2 className="frontend-title">Frontend</h2>
-          <div className="skill-img-wrapper">
+      <Card className="project-card">
+        <div className="skill-card-wrapper">
+        <h2 className="frontend-title">Frontend</h2>
+        <div className="skill-img-wrapper">
             <div className="skill-div">
 
-              <p>HTML5</p>
+            <p>HTML5</p>
             </div>
             <div className="skill-div">
 
-              <p>CSS3</p>
+            <p>CSS3</p>
             </div>
             <div className="skill-div">
 
-              <p>JAVASCRIPT</p>
+            <p>JAVASCRIPT</p>
             </div>
             <div className="skill-div">
 
-              <p>REACT</p>
+            <p>REACT</p>
             </div>
-          </div>
+        </div>
 
-          <h2 className="backend-title">Backend</h2>
+        <h2 className="backend-title">Backend</h2>
 
-          <h2 className="etc-title">Etc</h2>
-      </div>
+        <h2 className="etc-title">Etc</h2>
+        </div>
+      </Card>
+      
     </div>
   );
 };
