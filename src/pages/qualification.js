68a655319b83d4feea7925a749a59fdda0e8e24e
jungjin-lee90 <dlwjdwls60@naver.com>
2025-02-18 14:17:21 +0900

    자격증 정보 추가

--- src/pages/qualification.js
+++ src/pages/qualification.js
@@ -14,14 +14,24 @@ const Qualification = () => {
                 <h2 className="cert-title">Certification</h2>
                 <div className="cert-items-container">
                     <div className="cert-item">
-                    <img src={process.env.PUBLIC_URL + "/assets/images/q-net.png"} />
-                    <h3>tttt</h3>
-                    <h4>(한국산업인력공단. 2023.06.09)</h4>
+                    {/*<img src={process.env.PUBLIC_URL + "/assets/images/q-net.png"} />*/}
+                    <p className="link-wrapper">OCJP</p>
+                    <h4>(Oracle. 2013.07)</h4>
                     </div>
                     <div className="cert-item">
-                    <img src={process.env.PUBLIC_URL + "/assets/images/opic.webp"} />
-                    <h3>tttt</h3>
-                    <h4>(ACTFL. 2023.06.07)</h4>
+                    {/*<img src={process.env.PUBLIC_URL + "/assets/images/opic.webp"} />*/}
+                    <p className="link-wrapper">정보처리산업기사</p>
+                    <h4>(한국산업인력공단. 2013.11)</h4>
+                    </div>
+                    <div className="cert-item">
+                    {/*<img src={process.env.PUBLIC_URL + "/assets/images/opic.webp"} />*/}
+                    <p className="link-wrapper">리눅스마스터2급</p>
+                    <h4>(한국정보통신인력개발센터. 2013.12)</h4>
+                    </div>
+                    <div className="cert-item">
+                    {/*<img src={process.env.PUBLIC_URL + "/assets/images/opic.webp"} />*/}
+                    <p className="link-wrapper">AWS SAA</p>
+                    <h4>(AWS. 2023.11)</h4>
                     </div>
                 </div>
                 </Card>
