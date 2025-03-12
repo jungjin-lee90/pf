7c89121c7dbba48ac3e36c9c6cc11f766d073ec2
jungjin-lee90 <dlwjdwls60@naver.com>
2025-03-12 14:16:19 +0900

    멘트 변경

--- src/pages/home.js
+++ src/pages/home.js
@@ -24,11 +24,11 @@ const Home = () => {
       <div className="container home-container">
         <div className="title">
           <h1>{typedText && <span>{typedText}</span>}</h1>
-          <h2>BACK-END DEV.</h2>
+          <h2>Cloud engineer & Developer.</h2>
         </div>
         <div className="description">
-          <p>끊임없는 도전을 통해 문제를 해결하는 것을 즐깁니다.</p>
-          <p>지식의 습득과 성장에 열정을 가지고 있습니다.</p>
+          <p>끊임없는 도전을 통해 성장하는 것을 즐깁니다.</p>
+          <p>지식의 습득에 열정을 가지고 있습니다.</p>
         </div>
       </div>
       <div className="scroll">
